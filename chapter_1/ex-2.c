21bb30c4ad6ac6e4962b84f2a2f130434925ea52
Tanzina Chowdhury <tanzinatanny@gmail.com>
2018-07-14 15:11:02 +0600

    changes added to ex 2

--- chapter_1/ex-2.c
+++ chapter_1/ex-2.c
@@ -7,17 +7,18 @@ in meters, feet, inches and centimeters. */
 
 int main()
 {
-	distanceKm;
-	inMeters = distanceKm * 1000;	//1 km = 1000 m
-	inFeet = distanceKm * 3280.84;  // 1 km = 3280.84 ft
-	inCentimeters = distanceKm * 100000;	// 1 km = 100000 cm;
+	float distanceKm;
 
 	printf("Input the distace between the two cities in kilometers.\n");
 	scanf("%f", &distanceKm );
 
-	printf("The distance in meters is: \n", inMeters);
-	printf("The distance in feet is: \n", inFeet);
-	printf("The distance in centimeters is: \n", inCentimeters);
+	float inMeters = distanceKm * 1000;	//1 km = 1000 m
+	float inFeet = distanceKm * 3280.84;  // 1 km = 3280.84 ft
+	float inCentimeters = distanceKm * 100000;	// 1 km = 100000 cm;
+
+	printf("The distance in meters is: %f \n", inMeters);
+	printf("The distance in feet is: %f \n", inFeet);
+	printf("The distance in centimeters is: %f \n", inCentimeters);
 
 	return 0;
 
